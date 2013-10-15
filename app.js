db5da3d38f1ecf427cec549b213d0e676a489c61
Ricardo Alcocer <alco@ricardoalcocer.com>
2013-10-15 16:16:11 -0700

    Fixed require statements

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 */
 
 var _ = require('lodash');
-var settings=require('/lib/lilacs.js').getSettings();
-var parseActions=require('/lib/lilacsmod.js').parseActions;
+var settings=require('./lib/lilacs.js').getSettings();
+var parseActions=require('./lib/lilacsmod.js').parseActions;
 
 var ACS = require('acs').ACS;
 var ACS_KEY=settings.ACS_KEY;
